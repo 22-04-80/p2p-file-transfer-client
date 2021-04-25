7d7ea06caf45148186c2b63c01c5e9c7c5cf86b2
22-04-80 <s220480@student.uek.krakow.pl>
2021-04-25 14:57:53 +0200

    Log url loaded from env

--- src/channel/initChannel.ts
+++ src/channel/initChannel.ts
@@ -3,6 +3,7 @@ import {Channel, Socket} from "phoenix";
 const URL = process.env.REACT_APP_WS_URL;
 
 export function initChannel(): Channel {
+	console.log('url', URL)
 	const socket = new Socket(URL as string);
 	socket.connect();
 	return socket.channel("signal:relay", {});
